9d39e63d3a3dcba428a04749a71bad55da80efb8
Shawn Pivonka <shawnpivonka@Shawns-MBP.attlocal.net>
2020-04-09 07:25:40 -0500

    saving forr now, even though its not quite working

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-import http from "@/api/http";
+import http from '@/api/http';
 
 export const RealtimeStore = {
   pusher: null,
+  state: {},
   channels: [],
   setPusher(pusher) {
     this.pusher = pusher;
   },
-  getStore(store, channel_id) {
+  getChannel(store, channel_id) {
     return this.channels[store + '.' + channel_id];
   },
+  getStore(store, channel_id) {
+    if (!this.state || !this.state[store + '.' + channel_id]) {
+      return undefined;
+    }
+    return this.state[store][channel_id];
+    // return this.getChannel(store, channel_id).vuexStore.state[store];
+  },
+  getData(store, channel_id, prop, defaultData) {
+    if (this.getStore(store, channel_id) === undefined) {
+      if (defaultData !== undefined) {
+        return defaultData;
+      }
+      return undefined;
+    }
+    
+    return this.getStore(store, channel_id)[prop];
+  },
+  getCollectionData(store, channel_id, prop, defaultData) {
+    if (this.getData(store, channel_id) === undefined) {
+      if (defaultData !== undefined) {
+        return defaultData;
+      }
+      return undefined;
+    }
+  
+    return this.getData(store, channel_id, prop).data;
+  },
   getResource(store, channel_id, prop) {
-    return this.getStore(store, channel_id).resources[prop];
+    return this.getChannel(store, channel_id).resources[prop];
   },
-  getResourceState(store, channel_id, prop) {
-    return this.getPropResource(store, channel_id, prop).state;
-  },
-  subscribeToChannel(store, channel_id, vuexStore, options) {
+  subscribeToChannel(store, channel_id, options) {
     let channel_string = store + '.' + channel_id;
     if (!this.pusher) {
       console.error('Pusher is not setup yet: ', channel_string);
@@ -28,11 +53,9 @@ export const RealtimeStore = {
   
     const channel = this.pusher.subscribe(channel_string);
     this.channels[channel_string] = channel;
-    if (vuexStore) {
-      channel.vuexStore = vuexStore;
-    } else {
-      channel.store = {};
-    }
+    // if (vuexStore) {
+    //   channel.vuexStore = vuexStore;
+    // }
     
     channel.bind('event', payload => {
       this.processStoreChanges(payload.data);
@@ -47,48 +70,58 @@ export const RealtimeStore = {
     }
   
     return http().get(`${process.env.MIX_CLIENT_STORE_URL}/${store}/${channel_id}` + query).then(response => {
-      if (channel.store) {
-        channel.store = response.data;
-        return channel.store;
-      } else if (channel.vuexStore) {
+      this.state[store + '.' + channel_id] = response.data;
+      // this.state[store][channel_id] = response.data;
+      
+      if (channel.vuexStore) {
         channel.vuexStore.commit('initStore', {
           store: store,
           data: response.data
         });
-        channel.resources = [];
-        Object.keys(response.data).forEach((prop) => {
-          let url = `${process.env.MIX_CLIENT_STORE_URL}/${store}/${channel_id}/${prop}`;
-          channel.resources[prop] = {
-            channel: channel,
-            options: options,
-            appendNextPage() {
-              return http().get(url + '?size=2&offset=' + this.getCollectionLength()).then(response => {
-                response.data.data.forEach((item) => {
+      }
+      
+      channel.resources = [];
+      let internalMethods = this.internalStoreMethods;
+      let state = this.state[store + '.' + channel_id];
+      Object.keys(response.data).forEach((prop) => {
+        let url = `${process.env.MIX_CLIENT_STORE_URL}/${store}/${channel_id}/${prop}`;
+        channel.resources[prop] = {
+          channel: channel,
+          options: options,
+          appendNextPage() {
+            return http().get(url + '?size=10&offset=' + this.getCollectionLength()).then(response => {
+              response.data.data.forEach((item) => {
+                let payload = {prop: prop, data: item};
+                console.log('State: ', state, 'Payload: ', payload);
+                internalMethods.upsertCollection(state, payload);
+                if (channel.vuexStore) {
                   channel.vuexStore.commit('upsertCollection', {
                     store: store,
                     prop: prop,
                     data: item
                   });
-                });
-                
-                return response;
-              });
-            },
-            appendAll() {
-              this.appendNextPage().then(response => {
-                if (response.data.data.length) {
-                  this.appendAll();
                 }
               });
-            },
-            getCollectionLength() {
-              return this.channel.vuexStore.state[store][prop].data.length;
-            }
-          };
-        });
+          
+              return response;
+            });
+          },
+          appendAll() {
+            this.appendNextPage().then(response => {
+              if (response.data.data.length) {
+                this.appendAll();
+              }
+            });
+          },
+          getCollectionLength() {
+            // console.log('State: ', state[prop].data.length);
+            return state[prop].data.length;
+            // return this.channel.vuexStore.state[store][prop].data.length;
+          }
+        };
+      });
   
-        return response.data;
-      }
+      return response.data;
     });
   },
   unsubscribeToChannel (channel) {
@@ -120,7 +153,7 @@ export const RealtimeStore = {
         
       }, payload.delay);
     } else if (channel.store) {
-      this.internalStoreMethods[payload.method](channel.store, payload)
+      this.internalStoreMethods[payload.method](this.getStore(payload.store, payload.channel_id), payload)
     }
   },
   apiSuccessMiddleware (response) {
