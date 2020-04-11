d02960a64ab2cd2e1084ad329043beaedbd45771
Shawn Pivonka <shawnpivonka@Shawns-MBP.attlocal.net>
2020-04-11 11:42:12 -0500

    works now for store mutations or channel mutations

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ import http from '@/api/http';
 
 export const RealtimeStore = {
   pusher: null,
-  state: {},
+  Vue: null,
+  vuexStore: {},
   channels: [],
   setPusher(pusher) {
     this.pusher = pusher;
   },
+  setStore(store) {
+    this.vuexStore = store;
+  },
+  setVue(Vue) {
+    console.log('Setting Vue', Vue);
+    this.Vue = Vue;
+  },
   getChannel(store, channel_id) {
-    return this.channels[store + '.' + channel_id];
+    return this.channels[store + ':' + channel_id];
   },
   getResource(store, channel_id, prop) {
     return this.getChannel(store, channel_id).resources[prop];
   },
-  subscribeToChannel(store, channel_id, options, vuexStore) {
+  subscribeToChannel(store, channel_id, options) {
     let defaultOptions = {};
     if (options) {
       options = {...defaultOptions, ...options};
@@ -21,7 +29,7 @@ export const RealtimeStore = {
       options = defaultOptions;
     }
     
-    let channel_string = store + '.' + channel_id;
+    let channel_string = store + ':' + channel_id;
     if (!this.pusher) {
       console.error('Pusher is not setup yet: ', channel_string);
       return;
@@ -33,9 +41,6 @@ export const RealtimeStore = {
   
     const channel = this.pusher.subscribe(channel_string);
     this.channels[channel_string] = channel;
-    if (vuexStore) {
-      channel.vuexStore = vuexStore;
-    }
     
     channel.bind('event', payload => {
       this.processStoreChanges(payload.data);
@@ -49,15 +54,14 @@ export const RealtimeStore = {
       query += 'size=' + options.size;
     }
   
+    const vuexStore = this.vuexStore;
     return http().get(`${process.env.MIX_CLIENT_STORE_URL}/${store}/${channel_id}` + query).then(response => {
-      if (channel.vuexStore) {
-        channel.vuexStore.commit('initStore', {
-          store: store,
-          channel_id: channel_id,
-          data: response.data
-        });
-      }
-      
+      vuexStore.commit('initStore', {
+        store: store,
+        channel_id: channel_id,
+        data: response.data
+      });
+  
       channel.resources = [];
       Object.keys(response.data).forEach((prop) => {
         let url = `${process.env.MIX_CLIENT_STORE_URL}/${store}/${channel_id}/${prop}`;
@@ -67,15 +71,12 @@ export const RealtimeStore = {
           appendNextPage() {
             return http().get(url + '?offset=' + this.getCollectionLength()).then(response => {
               response.data.data.forEach((item) => {
-                // let payload = {prop: prop, data: item, channel_id: channel_id, store: store};
-                if (channel.vuexStore) {
-                  channel.vuexStore.commit('upsertCollection', {
-                    store: store,
-                    prop: prop,
-                    data: item,
-                    channel_id: channel_id
-                  });
-                }
+                vuexStore.commit('upsertCollection', {
+                  store: store,
+                  prop: prop,
+                  data: item,
+                  channel_id: channel_id
+                });
               });
           
               return response;
@@ -89,7 +90,8 @@ export const RealtimeStore = {
             });
           },
           getCollectionLength() {
-            return this.channel.vuexStore.state[store][channel_id][prop].data.length;
+            return vuexStore.state[store][prop].data.length;
+            // return vuexStore.state[store + ':' + channel_id][prop].data.length;
           }
         };
       });
@@ -99,34 +101,29 @@ export const RealtimeStore = {
   },
   unsubscribeToChannel (channel) {
     this.pusher.unsubscribe(channel);
-    if (channel.vuexStore) {
-      delete channel.vuexStore.state[channel.store][channel.channel_id];
-    }
+    delete this.vuexStore.state[channel.store + ':' + channel.channel_id];
     delete this.channels[channel];
   },
   processStoreChanges(payload) {
-    const channel = this.channels[payload.store + '.' + payload.channel_id];
+    const channel = this.channels[payload.store + ':' + payload.channel_id];
     
     /** Ignore channels you are not subscribed to */
     if (!channel) {
       console.log('Ignoring Channel: ', payload.channel);
       return;
     }
+  
+    setTimeout(() => {
+      if (payload.data) {
+        this.vuexStore.commit(payload.method, payload);
+      } else {
+        http().get(payload.api_call).then(res => {
+          payload.data = res.data;
+          this.vuexStore.commit(payload.method, payload);
+        });
+      }
     
-    /** If channel has a Vuex Store */
-    if (channel.vuexStore) {
-      setTimeout(() => {
-        if (payload.data) {
-          channel.vuexStore.commit(payload.method, payload);
-        } else {
-          http().get(payload.api_call).then(res => {
-            payload.data = res.data;
-            channel.vuexStore.commit(payload.method, payload);
-          });
-        }
-        
-      }, payload.delay);
-    }
+    }, payload.delay);
   },
   apiSuccessMiddleware (response) {
     if (response.data.clientStoreChanges) {
@@ -150,36 +147,68 @@ export const RealtimeStore = {
     
     return error;
   },
-  vuexMutations: {
+  channelMutations: {
     updateInCollection(state, payload) {
-      state[payload.store][payload.channel_id][payload.prop].data = state[payload.store][payload.channel_id][payload.prop].data.map(item => item.id === payload.data.id ? {...item, ...payload.data} : item);
+      state[payload.store + '.' + payload.channel_id][payload.prop].data = state[payload.store + '.' + payload.channel_id][payload.prop].data.map(item => item.id === payload.data.id ? {...item, ...payload.data} : item);
     },
     addToCollection(state, payload) {
-      let collection = state[payload.store][payload.channel_id][payload.prop].data;
+      let collection = state[payload.store + '.' + payload.channel_id][payload.prop].data;
       if (!collection.some(item => item.id === payload.data.id)) {
         collection.push(payload.data);
       }
     },
     upsertCollection(state, payload) {
-      let collection = state[payload.store][payload.channel_id][payload.prop].data;
+      let collection = state[payload.store + '.' + payload.channel_id][payload.prop].data;
       if (!collection.some(item => item.id === payload.data.id)) {
         collection.push(payload.data);
       } else {
-        state[payload.store][payload.channel_id][payload.prop].data = state[payload.store][payload.channel_id][payload.prop].data.map(item => item.id === payload.data.id ? {...item, ...payload.data} : item);
+        state[payload.store + '.' + payload.channel_id][payload.prop].data = state[payload.store + '.' + payload.channel_id][payload.prop].data.map(item => item.id === payload.data.id ? {...item, ...payload.data} : item);
       }
     },
     removeFromCollection(state, payload) {
-      state[payload.store][payload.channel_id][payload.prop].data = state[payload.store][payload.channel_id][payload.prop].data.filter(item => item.id !== payload.data.id);
+      state[payload.store + '.' + payload.channel_id][payload.prop].data = state[payload.store + '.' + payload.channel_id][payload.prop].data.filter(item => item.id !== payload.data.id);
     },
     setRoot(state, payload) {
-      state[payload.store][payload.channel_id][payload.prop].data = payload.data;
+      state[payload.store + '.' + payload.channel_id][payload.prop].data = payload.data;
     },
     initStore(state, payload) {
-      console.log('Payload: ', payload);
+      if (!state[payload.store]) {
+        // state[payload.store] = {};
+        RealtimeStore.Vue.set( state, [payload.store + '.' + payload.channel_id], {});
+      }
+      // state[payload.store + '.' + payload.channel_id] = payload.data;
+      RealtimeStore.Vue.set( state, [payload.store + '.' + payload.channel_id], payload.data);
+    }
+  },
+  storeMutations: {
+    updateInCollection(state, payload) {
+      state[payload.store][payload.prop].data = state[payload.store][payload.prop].data.map(item => item.id === payload.data.id ? {...item, ...payload.data} : item);
+    },
+    addToCollection(state, payload) {
+      let collection = state[payload.store][payload.prop].data;
+      if (!collection.some(item => item.id === payload.data.id)) {
+        collection.push(payload.data);
+      }
+    },
+    upsertCollection(state, payload) {
+      let collection = state[payload.store][payload.prop].data;
+      if (!collection.some(item => item.id === payload.data.id)) {
+        collection.push(payload.data);
+      } else {
+        state[payload.store][payload.prop].data = state[payload.store][payload.prop].data.map(item => item.id === payload.data.id ? {...item, ...payload.data} : item);
+      }
+    },
+    removeFromCollection(state, payload) {
+      state[payload.store][payload.prop].data = state[payload.store][payload.prop].data.filter(item => item.id !== payload.data.id);
+    },
+    setRoot(state, payload) {
+      state[payload.store][payload.prop].data = payload.data;
+    },
+    initStore(state, payload) {
       if (!state[payload.store]) {
         state[payload.store] = {};
       }
-      state[payload.store][payload.channel_id] = payload.data;
+      state[payload.store] = payload.data;
     }
   },
 };
